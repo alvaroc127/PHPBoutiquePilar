f4e72982b0e7faa5c5109012eaf0de8003d9312e
alvaroc127 <acoronador1@ucentral.edu.co>
2018-04-09 11:07:39 -0500

    MOdificacion del footer para que se contraiga

--- mod/footer.php
+++ mod/footer.php
@@ -1,18 +1,20 @@
 <footer id="pieP">
 <hr id="separador"/>
-<div id="boxPie">
-<div id="linker">
+<div id="boxPie" class="row">
+<div id="linker" class="col-sm-4">
 <ul>
 <li><a href=""> Adminsitracion</a></li>
 <li><a href="">derechos reservados</a></li>
 <li><a href="">¿Quines Somos?</a></li>
 </ul>
 </div>
-<div id="mrInfo">
+<div id="mrInfo" class="col-sm-auto">
 <figure>
-<img id="imgFacebook" src="img/facebook.png" alt="facebooklink">
-<img id="imgTwiter" src="img/twiter.png" alt="twiterlink">
-<img id="imginstgram" src="img/instagram.png" alt="instagramlink">
+    <ul>
+<li><img id="imgFacebook" src="img/facebook.png" alt="facebooklink"></li>
+<li><img id="imgTwiter" src="img/twiter.png" alt="twiterlink"></li>
+<li><img id="imginstgram" src="img/instagram.png" alt="instagramlink"></li>
+    </ul>
 </figure>
 <p>Direcci&oacute;n: Cr 24 n 9-20</p>
 <p>Tel&eacute;fono: 3177573723</p>
