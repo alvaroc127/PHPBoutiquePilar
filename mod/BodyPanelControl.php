639ee17deb9259ee78b128704a9b18f6032d5ad4
alvaroc127 <acoronador1@ucentral.edu.co>
2018-04-17 16:39:27 -0500

    Modificaciones mision y vision

--- mod/BodyPanelControl.php
+++ mod/BodyPanelControl.php
@@ -27,37 +27,254 @@ function loadFunction(){//abrir el directorio donde estan las images y comenzar 
         <div class="col-md-8"> <h2 id="nombrePan" class="text-left">Panel de Control</h2>   
             </div>
         <div class="col-md-2  colPri"> 
-            <p><a href="salida.php">salir</a></p><!-- aqui va el usuario-->
+            <p><a  class="page-link" href="salida.php">salir</a></p><!-- aqui va el usuario-->
         </div>
             <div class="col-md-2  colPri">   
                 <p><?php echo $contU->getemail()?></p><!-- aqui va el usuario-->
             </div>
     </div>
-        <div class="row">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+        <div class="card-columns">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Crear Producto</h4>
+                    <figure>
+                        <img class="card-img-top img-fluid w-50" src="./img/pen_14403.png" alt="un ejemplo">
+                    </figure>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Aqui creara los productos que desea</p>
+                </div>
+                <footer class="card-footer text-muted">
+                    <!--un boton y otras cositas.-->
+                    <a href="" class="btn btn-primary">crearProducto</a>
+                    <p class="card-text"><small class="text-muted">peque textp</small></p>
+                </footer>
+            </div>
+            <div class="card" id="PCcard2">
+                <div class="card-header">
+                    <h4 class="card-title">Modificar Producto</h4>
+                    <figure>
+                        <img class="card-img-top img-fluid w-50" src="./img/upload_arrow_14448.png" alt="un ejemplo">
+                    </figure>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Aqui Modificara sus productos</p>
+                </div>
+                <footer class="card-footer text-muted">
+                    <!--un boton y otras cositas.-->
+                    <a href="" class="btn btn-primary">Modificar Producto</a>
+                    <p class="card-text"><small class="text-muted">peque textp</small></p>
+                </footer>
+            </div>
+            
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Buscar Producto</h4>
+                    <figure>
+                        <img class="card-img-top img-fluid w-50" src="./img/binoculars_Buscar.png" alt="un ejemplo">
+                    </figure>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Aqui buscara sus productos</p>
+                </div>
+                <footer class="card-footer text-muted">
+                    <!--un boton y otras cositas.-->
+                    <a href="" class="btn btn-primary">Buscar Producto</a>
+                    <p class="card-text"><small class="text-muted">peque textp</small></p>
+                </footer>
+            </div>
         </div>
-        <div class="row">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+       
+        <div class="card-columns">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Crear Producto</h4>
+                    <figure>
+                        <img class="card-img-top img-fluid w-50" src="./img/pen_14403.png" alt="un ejemplo">
+                    </figure>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Aqui creara los productos que desea</p>
+                </div>
+                <footer class="card-footer text-muted">
+                    <!--un boton y otras cositas.-->
+                    <a href="" class="btn btn-primary">crearProducto</a>
+                    <p class="card-text"><small class="text-muted">peque textp</small></p>
+                </footer>
+            </div>
+            <div class="card" id="PCcard2">
+                <div class="card-header">
+                    <h4 class="card-title">Modificar Producto</h4>
+                    <figure>
+                        <img class="card-img-top img-fluid w-50" src="./img/upload_arrow_14448.png" alt="un ejemplo">
+                    </figure>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Aqui Modificara sus productos</p>
+                </div>
+                <footer class="card-footer text-muted">
+                    <!--un boton y otras cositas.-->
+                    <a href="" class="btn btn-primary">Modificar Producto</a>
+                    <p class="card-text"><small class="text-muted">peque textp</small></p>
+                </footer>
+            </div>
+            
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Buscar Producto</h4>
+                    <figure>
+                        <img class="card-img-top img-fluid w-50" src="./img/binoculars_Buscar.png" alt="un ejemplo">
+                    </figure>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Aqui buscara sus productos</p>
+                </div>
+                <footer class="card-footer text-muted">
+                    <!--un boton y otras cositas.-->
+                    <a href="" class="btn btn-primary">Buscar Producto</a>
+                    <p class="card-text"><small class="text-muted">peque textp</small></p>
+                </footer>
+            </div>
         </div>
-        <div class="row">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+        
+        <div class="card-columns">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Crear Producto</h4>
+                    <figure>
+                        <img class="card-img-top img-fluid w-50" src="./img/pen_14403.png" alt="un ejemplo">
+                    </figure>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Aqui creara los productos que desea</p>
+                </div>
+                <footer class="card-footer text-muted">
+                    <!--un boton y otras cositas.-->
+                    <a href="" class="btn btn-primary">crearProducto</a>
+                    <p class="card-text"><small class="text-muted">peque textp</small></p>
+                </footer>
+            </div>
+            <div class="card" id="PCcard2">
+                <div class="card-header">
+                    <h4 class="card-title">Modificar Producto</h4>
+                    <figure>
+                        <img class="card-img-top img-fluid w-50" src="./img/upload_arrow_14448.png" alt="un ejemplo">
+                    </figure>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Aqui Modificara sus productos</p>
+                </div>
+                <footer class="card-footer text-muted">
+                    <!--un boton y otras cositas.-->
+                    <a href="" class="btn btn-primary">Modificar Producto</a>
+                    <p class="card-text"><small class="text-muted">peque textp</small></p>
+                </footer>
+            </div>
+            
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Buscar Producto</h4>
+                    <figure>
+                        <img class="card-img-top img-fluid w-50" src="./img/binoculars_Buscar.png" alt="un ejemplo">
+                    </figure>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Aqui buscara sus productos</p>
+                </div>
+                <footer class="card-footer text-muted">
+                    <!--un boton y otras cositas.-->
+                    <a href="" class="btn btn-primary">Buscar Producto</a>
+                    <p class="card-text"><small class="text-muted">peque textp</small></p>
+                </footer>
+            </div>
         </div>
-        <div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+    
+        <div class="card-columns">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Crear Producto</h4>
+                    <figure>
+                        <img class="card-img-top img-fluid w-50" src="./img/pen_14403.png" alt="un ejemplo">
+                    </figure>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Aqui creara los productos que desea</p>
+                </div>
+                <footer class="card-footer text-muted">
+                    <!--un boton y otras cositas.-->
+                    <a href="" class="btn btn-primary">crearProducto</a>
+                    <p class="card-text"><small class="text-muted">peque textp</small></p>
+                </footer>
+            </div>
+            <div class="card" id="PCcard2">
+                <div class="card-header">
+                    <h4 class="card-title">Modificar Producto</h4>
+                    <figure>
+                        <img class="card-img-top img-fluid w-50" src="./img/upload_arrow_14448.png" alt="un ejemplo">
+                    </figure>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Aqui Modificara sus productos</p>
+                </div>
+                <footer class="card-footer text-muted">
+                    <!--un boton y otras cositas.-->
+                    <a href="" class="btn btn-primary">Modificar Producto</a>
+                    <p class="card-text"><small class="text-muted">peque textp</small></p>
+                </footer>
+            </div>
+            
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Buscar Producto</h4>
+                    <figure>
+                        <img class="card-img-top img-fluid w-50" src="./img/binoculars_Buscar.png" alt="un ejemplo">
+                    </figure>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Aqui buscara sus productos</p>
+                </div>
+                <footer class="card-footer text-muted">
+                    <!--un boton y otras cositas.-->
+                    <a href="" class="btn btn-primary">Buscar Producto</a>
+                    <p class="card-text"><small class="text-muted">peque textp</small></p>
+                </footer>
+            </div>
         </div>
-        <div>
-            <div></div>
+    
+        <div class="card-columns">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Crear Producto</h4>
+                    <figure>
+                        <img class="card-img-top img-fluid w-50" src="./img/pen_14403.png" alt="un ejemplo">
+                    </figure>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Aqui creara los productos que desea</p>
+                </div>
+                <footer class="card-footer text-muted">
+                    <!--un boton y otras cositas.-->
+                    <a href="" class="btn btn-primary">crearProducto</a>
+                    <p class="card-text"><small class="text-muted">peque textp</small></p>
+                </footer>
+            </div>
+            <div class="card" id="PCcard2">
+                <div class="card-header">
+                    <h4 class="card-title">Modificar Producto</h4>
+                    <figure>
+                        <img class="card-img-top img-fluid w-50" src="./img/upload_arrow_14448.png" alt="un ejemplo">
+                    </figure>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Aqui Modificara sus productos</p>
+                </div>
+                <footer class="card-footer text-muted">
+                    <!--un boton y otras cositas.-->
+                    <a href="" class="btn btn-primary">Modificar Producto</a>
+                    <p class="card-text"><small class="text-muted">peque textp</small></p>
+                </footer>
+            </div>
+            
         </div>
+    
 </section>
--- mod/BodyQuienesSomos.php
+++ mod/BodyQuienesSomos.php
@@ -1,43 +1,49 @@
-<section  class="col-xl-auto classmid">
-    <article class="row rowcont">
-        <h2> Misi&oacute;n</h2>
-        <div class="col-md-6 colvisio">
-        <figure>
-            <img id="imagenMision" src="img/ropaXfile.jpg" atl="imagen_mision" class="img-fluid">
-            <figcaption>
-                
-            </figcaption>
-        </figure>
-        </div>
-        <div class="col-md-6 colvisio">
-        <p> Comercializar ropa de alta calidad, 
+<section class="card-columns">
+    <article class="card">
+        <header class="card-header">
+            <figure>
+                <img  src="img/ropaXfile.jpg" atl="imagen_mision" class="card-img-top img-fluid">
+                <p class="card-text"><figcaption class="text-muted">misi&oacute;n
+                </figcaption>
+            </figure>
+            <h2 class="card-title"> Misi&oacute;n</h2>
+        </header>
+        <div class="card-body">
+            <p class="card-text"> Comercializar ropa de alta calidad, 
             cumpliendo con los precios del mercado, 
             satisfaciendo a los clientes con diseño y estilo.
         </p>
         </div>
     </article>
     
-    <article class="row rowcont"> 
-        <h2> Visi&oacute;n</h2>
-        <div class="col-md-6 colvisio">
-        <figure>
-            <img id="imagenVision" src="img/vision.jpg" alt="imagen_vision" class="img-fluid" >
-            <figcaption> </figcaption>
-        </figure>
+    <article class="card"> 
+        <header class="card-header">
+                <figure>
+                    <img  src="img/vision.jpg" alt="imagen_vision" class="card-img-top img-fluid" >
+                    <p class="card-text"><figcaption class="text-muted">visi&oacute;n</figcaption>
+                </figure>
+            <h2 class="card-title"> Visi&oacute;n</h2>
+        </header>
+        <div class="card-body">
+            <p class="card-text"> 
+                Para el año 2020 convertirnos en una de las  empresas mas fuerte de bogot&aacute;, 
+            siempre cumpliendo con los acuerdos  que regulan nuestro negocio y sus alcances. 
+             </p>
            </div> 
-        <div class="col-md-6 colvisio">
-        <p> 
-            Para el año 2020 convertirnos en una empresa con una s&oacute;lida econom&iacute;ca y modelo de negocio, 
-            con un alto margen de ganancias, 
-            siempre cumpliendo con los acuerdos y leyes que regulan el negocio y sus alcances. 
-        </p>
-        </div>
-    
     </article>
     
-    <article class="row rowcont"> 
-        <h2>Valores</h2>
-        <p> </p>
-    
+    <article class="card"> 
+        <header class="card-header">
+                <figure>
+                    <img  src="img/vision.jpg" alt="imagen_vision" class="card-img-top img-fluid" >
+                    <p class="card-text"><figcaption class="text-muted">valores</figcaption>
+                </figure>
+            <h2 class="card-title"> Valores</h2>
+        </header>
+        <div class="card-body">
+            <p class="card-text"> 
+                aqui estan nuestros valores
+             </p>
+           </div>
     </article>
 </section>
